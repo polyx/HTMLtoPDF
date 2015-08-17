123f2c97e04df878229c34f7a9b53a44e28dc88e
Dmitry <filosofov.dmitry@gmail.com>
2015-08-17 21:59:37 +0200

    some refactoring

--- HTMLtoPDFRederer.py
+++ HTMLtoPDFRederer.py
@@ -1,20 +1,11 @@
 import pdfkit
 
 
+class ToPDF:
+    def __init__(self, doc, css):
+        self.doc = doc
+        self.css = css
 
-doc = ''
-with open('content.html') as f:
-   doc = f.read()
-
-css = ''
-with open('crayon.css') as f:
-   css = f.read()
-
-doc = '<html><head></head><body>' + doc + '</body></html>'
-
-with open('newDOC.html', 'w') as f:
-    f.write(doc)
-
-pdfkit.from_string(doc,output_path='out.pdf', css='crayon.css')
-
-
+    def convert(self, output='out.pdf'):
+        self.doc = '<html><head></head><body>' + self.doc + '</body></html>'
+        pdfkit.from_string(self.doc, output_path=output, css=self.css)
