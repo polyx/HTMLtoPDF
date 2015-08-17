646ed2f8e74b6163eac913b09e02e95974aa0880
Dmitry <filosofov.dmitry@gmail.com>
2015-08-17 21:12:46 +0200

    parse cmd testing

--- HTMLtoPDFRederer.py
+++ HTMLtoPDFRederer.py
@@ -14,6 +14,7 @@ doc = '<html><head></head><body>' + doc + '</body></html>'
 
 with open('newDOC.html', 'w') as f:
     f.write(doc)
-pdfkit.from_string(doc,output_path='pdfka2.pdf', css='crayon.css')
+
+pdfkit.from_string(doc,output_path='out.pdf', css='crayon.css')
 
 
--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 import config
 import requests
-import HTMLtoPDFRederer
+#import HTMLtoPDFRederer
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('URL', help='URL with article to be converted to pdf article')
+parser.add_argument('-o', '--output', help='Output file name without extension(Always pdf)' )
+args = parser.parse_args()
+
+if args.output:
+    print(args.output)
+else:
+    print(args.URL)
 
 
-req_url = 'https://readability.com/api/content/v1/parser'
-args = {
-    'url' : config.settings.url,
-    'token' : config.settings.token
-}
-response = requests.get(req_url, args)
-if response.status_code == requests.codes.ok:
-    print("Response recieved.")
-    json_resp = response.json()
-    content = json_resp['content']
+# req_url = 'https://readability.com/api/content/v1/parser'
+# args = {
+#     'url' : config.settings.url,
+#     'token' : config.settings.token
+# }
+# response = requests.get(req_url, args)
+# if response.status_code == requests.codes.ok:
+#     print("Response recieved.")
+#     json_resp = response.json()
+#     content = json_resp['content']
+
+
 #DEBUG help
 # with open('content.html','w') as f:
 #     f.write(content)
