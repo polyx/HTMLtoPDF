6f0c8452df1426f52d8316b80b52896b9d1ee737
Dmitry <filosofov.dmitry@gmail.com>
2015-08-19 15:04:44 +0200

    More refactoring

--- main.py
+++ main.py
@@ -3,34 +3,41 @@ import requests
 import HTMLtoPDFRederer
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument('URL', help='URL with article to be converted to pdf article')
-parser.add_argument('-o', '--output', help='Output file name without extension(Always pdf)' )
-args = parser.parse_args()
+class ArticleRequester:
+    def __init__(self, url=config.settings['url']):
 
-if args.output:
-    print(args.output)
-else:
-    print(args.URL)
-
-
-req_url = 'https://readability.com/api/content/v1/parser'
-args = {
-    'url' : config.settings.url,
-    'token' : config.settings.token
-}
-response = requests.get(req_url, args)
-if response.status_code == requests.codes.ok:
-    print("Response recieved.")
-    json_resp = response.json()
-    content = json_resp['content']
+        self.req_url = 'https://readability.com/api/content/v1/parser'
+        self.args = {
+            'token' : config.settings['token'],
+            'url' : url
+        }
 
 
-#DEBUG help
-with open('etc/content.html','w') as f:
-   f.write(content)
-with open("etc/response.json", 'w') as response_json:
-   response_json.write(response.text)
-   response_json.close()
+    def getArticle(self):
+        response = requests.get(self.req_url, args)
+
+        if response.status_code == requests.codes.ok:
+            print("Response recieved. CODE: " + response.status_code)
+            json_resp = response.json()
+            content = json_resp['content']
+
+        return content
 
 
+    def writeDebugFiles(self):
+        with open('etc/content.html','w') as f:
+            f.write(self.content)
+        with open("etc/response.json", 'w') as response_json:
+            response_json.write(self.response.text)
+            response_json.close()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('URL', help='URL with article to be converted to pdf article')
+    parser.add_argument('-o', '--output', help='Output file name without extension(Always pdf)' )
+    args = parser.parse_args()
+    if args.output:
+        print(args.output)
+    else:
+        print(args.URL)
